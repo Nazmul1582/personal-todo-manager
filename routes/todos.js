8a2eb1c96bd78b17f5f288943dbb2445aefdcedc
Nazmul1582 <nazmulhasan1582@gmail.com>
2026-02-20 23:45:44 +0600

    created the create todo route

--- routes/todos.js
+++ routes/todos.js
@@ -14,9 +14,44 @@ router.get("/todos", async (req, res) => {
     const filteredTodos = todos.filter((item) => item.userId === userId)
     return res.status(200).json(filteredTodos)
   } catch (error) {
+    console.log("error", error)
     if (error.code === "ENOENT") {
       return res.status(404).json({ message: "No such file" })
-    } else return res.status(400).json({ message: "error" })
+    } else return res.status(400).json({ message: error })
+  }
+})
+
+router.post("/todo/create", async (req, res) => {
+  try {
+    const { text } = req.body
+    if (!text) {
+      return res
+        .status(400)
+        .json({ message: "You must be provide the todo's content" })
+    }
+    const userId = req.cookies.userId
+    const createdAt = new Date().toISOString()
+
+    const data = await fs.readFile(todosFile, "utf8")
+    let todos = JSON.parse(data)
+    const todoId = `t${todos.length + 1}`
+
+    const newTodo = {
+      todoId,
+      userId,
+      text,
+      createdAt,
+    }
+
+    todos.push(newTodo)
+
+    await fs.writeFile(todosFile, JSON.stringify(todos, null, 2))
+    res
+      .status(201)
+      .json({ message: "New todo created successfully!", data: newTodo })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ message: error.message })
   }
 })
 
